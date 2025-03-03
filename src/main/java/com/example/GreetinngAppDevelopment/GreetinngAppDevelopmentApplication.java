32f9764353a305a8160baee39b68207ec1474ead
Saniya Gupta <saniya.gupta_cs21@gla.ac.in>
2025-03-03 16:04:29 +0530

    deleted message

---
+++ src/main/java/com/example/GreetinngAppDevelopment/GreetinngAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreetinngAppDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetinngAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetinngAppDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreetinngAppDevelopment/GreetinngAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreetinngAppDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetinngAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
