b5e6cd49d10a677db98033bb3179c026bd943927
Samiksha <maahit925@gmail.com>
2025-03-03 14:45:12 +0530

    for uc5

--- GreetingApp/src/main/java/com/GreetingApp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/GreetingApp/controller/GreetingController.java
@@ -3,12 +3,10 @@ package com.GreetingApp.controller;
 import com.GreetingApp.model.Greeting;
 import com.GreetingApp.repository.GreetingRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-
-
-
-
+import java.util.Optional;
 @RestController
 @RequestMapping("/greetings")
 public class GreetingController {
@@ -27,5 +25,16 @@ public class GreetingController {
     @GetMapping
     public List<Greeting> getAllGreetings() {
         return greetingRepository.findAll();
+    }
+//    @GetMapping("/{id}")
+//    public Greeting getGreetingById(@PathVariable Long id) {
+//        return greetingRepository.findById(id)
+//                .orElseThrow(() -> new RuntimeException("Greeting not found with ID: " + id));
+//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Greeting> getGreetingById(@PathVariable Long id) {
+        Optional<Greeting> greeting = greetingRepository.findById(id);
+        return greeting.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
