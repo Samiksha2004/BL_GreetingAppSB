6044fbf6e63e68c3531c1a40bb54d8179fa4c774
Samiksha2004 <94538487+Samiksha2004@users.noreply.github.com>
2025-03-03 18:14:44 +0530

    Delete GreetingAppDevelopment directory

--- GreetingAppDevelopment/src/main/java/com/GreetingAppDevelopment/service/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package com.GreetingAppDevelopment.service;
-
-import org.springframework.stereotype.Service;
-@Service
-public class GreetingService {
-    public String getGreetingMessage() {
-        return "Hello World";
-    }
-}
